cb5315b56db66eba1a9049cebd0042236862a9ab
spajeo <scarlettpajeo@gmail.com>
2017-01-10 23:01:36 +0100

    42 position de line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: Scarlett <Scarlett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 23:07:45 by spajeo            #+#    #+#             */
-/*   Updated: 2017/01/08 14:32:49 by Scarlett         ###   ########.fr       */
+/*   Updated: 2017/01/10 22:57:40 by spajeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@ int		get_next_line (const int fd, char **line)
 	int			i; 
 	int 			t_len;
 	int static		STARTED = 0;
-	int static		start = 0;
+	int static		start;
 
 	i = 0;
 	t_len = 0;
+	*line = ft_strnew(0);
 	while (1)
 	{
 		if (!ret)
@@ -35,47 +36,34 @@ int		get_next_line (const int fd, char **line)
 				return (-1);
 			if (!ret)
 				return (0);
-			*line = ft_strnew(0);
 			STARTED = 1;
 		}
 		t_len = ft_strlen_c_len(&buff[start], '\n', ret);
-		*line = ft_strjoin(*line, ft_memcpy(ft_memalloc(t_len), &buff[start], t_len));
+		*line = ft_strjoin(*line, 
+				ft_memcpy(ft_memalloc(t_len), &buff[start], t_len));
 		/* deux caractere inutiles imprimes, mauvaise dimention pour line */
 		ret = ret - t_len;
 		if (buff[start = start + t_len] == '\n')
 		{
-			printf("[ret final = %d buff[start == %d] = %d]\n", ret, start,  buff[start] );
 			ret--;
 			start++;	
-			printf("[return(1) ret = %d buff[start == %d] = %c]\n", ret, start, buff[start]);
 			return (1);
 		}
-		printf("no return >>> [start = %d][buff[start] = %c]\n", start, buff[start] );
 	}
 }
+
 int		main (int argc, char **argv)
 {
 	int		fd;
-	char		*line;
+	char* str;
+	int j;
 
-	if (argc > 1)
+	fd = open(argv[1], O_RDONLY);
+	while ((j = get_next_line(fd, &str)) > 0)
 	{
-		if ((argc > 2 || (fd = open(argv[1], O_RDONLY)) != -1))
-		{
-			if (argc > 3)
-				printf("%d", get_next_line(23, &line));
-			else
-			{
-				if (argc > 2)
-					fd = 0;
-				while (get_next_line(fd, &line) == 1)
-				{
-					ft_putstr(line);
-					ft_strdel(&line);
-				}
-				close(fd);
-			}
-		}
-		return (0);
+		ft_putstr(str);
+		free(str);
+		str = 0;
 	}
+	return (0);
 }
