01dad3b965907522ea231c06fae227c7c51f36eb
spajeo <scarlettpajeo@gmail.com>
2017-01-16 21:44:52 +0100

    (au milieu du multi fd)

--- get_next_line.c
+++ get_next_line.c
@@ -1,25 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spajeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/16 15:01:51 by spajeo            #+#    #+#             */
+/*   Updated: 2017/01/16 21:20:00 by spajeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int		get_next_line (const int fd, char **line)
+static void		*ft_lstiter(const int fd, static t_gnl *NODE, int free)
 {
- 	t_gnl static NODE;
+	t_gnl *first;
+	t_gnl *tmp;
+	t_gnl *tmp2;
 
+	first = NODE
+	if (!free)
+	{	
+		next->prev = PREV;
+		prev->next = NEXT;
+		free(NODE);
+		return ;
+	}
+	if (FD != fd)
+		tmp = NEXT;
+	while (fd != FD && tmp != NODE)
+		tmp = tmp->next;
+	if (fd == tmp->fd)
+		return ;
+	else if (tmp == NODE)
+	{
+		if (!(NODE = (t_list *)malloc(sizeof(t_list))))
+					return (NULL);
+			FD = fd;
+			return ;
+	}
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	t_gnl static *NODE = NULL;
+
+	ft_lstiter(fd, NODE, 1)
 	if (fd < 0 || !line || !(*line = ft_strnew(0)))
 		return (-1);
 	while (1)
 	{
-		if (!RET && !(START = 0) && 
-		((RET = read(fd, BUFF, BUFF_SIZE)) < 0))
+		if (!RET && !(START = 0) &&
+				((RET = read(fd, BUFF, BUFF_SIZE)) < 0))
 			return (-1);
 		if (!RET && **line)
 			return (1);
 		if (!RET)
-			return (0);
+			if (ft_lstiter(FD, NODE, 0))
+				return (0);
 		LEN = ft_strlen_c_len(&BUFF[START], EOL, RET);
-		*line = ft_strjoin_free(*line, 
-			ft_memcpy(ft_strnew(LEN), &BUFF[START], LEN));
+		*line = ft_strjoin_free(*line,
+				ft_memcpy(ft_strnew(t.len), &BUFF[START], LEN));
 		if ((RET -= LEN) && (BUFF[START += LEN] == EOL))
-		{ 
+		{
 			RET--;
 			START++;
 			return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spajeo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/16 17:20:33 by spajeo            #+#    #+#             */
+/*   Updated: 2017/01/16 21:43:48 by spajeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -11,20 +22,26 @@
 # define BUFF_SIZE 1
 # define EOL '\n'
 
-int             get_next_line (const int fd, char **line);
+int				get_next_line (const int fd, char **line);
 
-typedef struct titi
+typedef struct	s_titi
 {
-       	char	buff[BUFF_SIZE];
-        int	ret;
-        int     start;
-        int     len;	
+	char		buff[BUFF_SIZE];
+	int			ret;
+	int			start;
+	int			len;
+	int			fd;
+	int			*prev;
+	int			*next;
 }				t_gnl;
 
 # define NODE	t
-# define BUFF	t.buff
-# define RET	t.ret
-# define START	t.start
-# define LEN	t.len
+# define BUFF	t->buff
+# define START	t->start
+# define LEN	t->len
+# define RET	t->ret
+# define FD		t->fd
+# define PREV	t->prev;
+# define NEXT	t->next
 
 #endif
