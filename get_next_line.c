42d85672beecc89a1ba18c008318f21432ce299c
spajeo <scarlettpajeo@gmail.com>
2017-01-24 01:35:17 +0100

    (supporte LEN == 0)

--- get_next_line.c
+++ get_next_line.c
@@ -6,61 +6,31 @@
 /*   By: spajeo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 15:01:51 by spajeo            #+#    #+#             */
-/*   Updated: 2017/01/16 22:55:17 by spajeo           ###   ########.fr       */
+/*   Updated: 2017/01/24 01:30:11 by spajeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_gnl		*ft_lst(const int fd, static t_gnl **node, int ffree)
+int				get_next_line(const int fd, char **line)
 {
-	if (!ffree)
-	{	
-		if (*node->next)
-			node->next->prev = node->prev;
-		if (node->prev)
-			node->prev->next = node->next;
-		free(node);
-		return 1;
-	}
-	while (FD != fd PREV != NULL) 
-		node = PREV;
-	while (FD != fd && NEXT != NULL)
-		node = NEXT;
-	if (fd == FD)
-	{
-		return (2);
-	}
-	if (NEXT == NULL)
-	{
-		tmp = node->prev;
-		if (!(NODE = (t_list *)malloc(sizeof(t_list))))
-					return (NULL);
-			FD = fd;
-			return (1);
-	}
-}
+	static t_gnl node;
 
-int				one_line(const int fd, char **line, t_gnl *NODE)
-{
-//i	t_gnl static *NODE = NULL;
-
-	if (fd < 0 || !line || !(*line = ft_strnew(0)))
+	if (fd < 0 || !line  || !(*line = ft_strnew(0)))
 		return (-1);
-	while (1)
+	while (42)
 	{
-		if (!RET && !(START = 0) &&
+		if (RET <= 0 && !(START = 0) &&
 				((RET = read(fd, BUFF, BUFF_SIZE)) < 0))
 			return (-1);
 		if (!RET && **line)
 			return (1);
 		if (!RET)
-//			if (ft_lst(FD, NODE, 0))
-				return (0);
+			return (0);
 		LEN = ft_strlen_c_len(&BUFF[START], EOL, RET);
 		*line = ft_strjoin_free(*line,
-				ft_memcpy(ft_strnew(t.len), &BUFF[START], LEN));
-		if ((RET -= LEN) && (BUFF[START += LEN] == EOL))
+				ft_memcpy(ft_strnew(LEN), &BUFF[START], LEN));
+		if ((RET -= LEN) && ((BUFF[START += LEN] == EOL) || !LEN))
 		{
 			RET--;
 			START++;
@@ -68,12 +38,3 @@ int				one_line(const int fd, char **line, t_gnl *NODE)
 		}
 	}
 }
-
-int				get_next_line(const int fd, char **line)
-{
-	static t_gnl	*begin = NULL;
-	t_gnl	*node;
-
-	node = ft_lst(fd, &begin, 1)
-}
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: spajeo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 17:20:33 by spajeo            #+#    #+#             */
-/*   Updated: 2017/01/16 22:50:19 by spajeo           ###   ########.fr       */
+/*   Updated: 2017/01/23 20:55:43 by spajeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 # include <fcntl.h>
 # include <stdio.h>
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10000
 # define EOL '\n'
 
-int				get_next_line (const int fd, char **line);
+int					get_next_line (const int fd, char **line);
 
 typedef struct		s_titi
 {
@@ -30,18 +30,11 @@ typedef struct		s_titi
 	int				ret;
 	int				start;
 	int				len;
-	int				fd;
-	t_gnl			*prev;
-	t_gnl			*next;
 }					t_gnl;
 
-# define NODE	t
-# define BUFF	t->buff
-# define START	t->start
-# define LEN	t->len
-# define RET	t->ret
-# define FD		t->fd
-# define PREV	t->prev;
-# define NEXT	t->next
+# define BUFF	node.buff
+# define START	node.start
+# define LEN	node.len
+# define RET	node.ret
 
 #endif
